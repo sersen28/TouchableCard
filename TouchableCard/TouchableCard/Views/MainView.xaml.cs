f5c4e3ff3dec83561fe9addab57d189cdb74782f
김태훈 <sersen28@daum.net>
2024-04-04 11:02:40 +0900

    소스코드 구조 정리

---
+++ TouchableCard/TouchableCard/Views/MainView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using TouchableCard.ViewModels;
+
+namespace TouchableCard
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainView : Window
+	{
+		public MainView()
+		{
+			InitializeComponent();
+			this.DataContext = new MainViewModel();
+		}
+	}
+}
