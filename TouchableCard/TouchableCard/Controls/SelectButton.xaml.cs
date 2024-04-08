f15c3e7ddf8a9b91a1b83a315ff6fff98622149c
김태훈 <sersen28@daum.net>
2024-04-08 15:09:45 +0900

    SelectButton(Radio) 구현

---
+++ TouchableCard/TouchableCard/Controls/SelectButton.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TouchableCard.Controls
+{
+	/// <summary>
+	/// SelectButton.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class SelectButton : RadioButton
+	{
+		public SelectButton()
+		{
+			InitializeComponent();
+		}
+
+		/*
+		public static readonly DependencyProperty GroupNameProperty
+			= DependencyProperty.Register("GroupName", typeof(string)
+				, typeof(SelectButton));
+
+		public string GroupName
+		{
+			get => (string)GetValue(GroupNameProperty);
+			set => SetValue(GroupNameProperty, value);
+		}
+		*/
+
+		public static readonly DependencyProperty NormalSourceProperty
+			= DependencyProperty.Register("NormalSource", typeof(ImageSource)
+				, typeof(SelectButton));
+
+		public ImageSource NormalSource
+		{
+			get => (ImageSource)GetValue(NormalSourceProperty);
+			set => SetValue(NormalSourceProperty, value);
+		}
+
+		public static readonly DependencyProperty CheckedSourceProperty
+			= DependencyProperty.Register("CheckedSource", typeof(ImageSource)
+				, typeof(SelectButton));
+
+		public ImageSource CheckedSource
+		{
+			get => (ImageSource)GetValue(CheckedSourceProperty);
+			set => SetValue(CheckedSourceProperty, value);
+		}
+	}
+}
